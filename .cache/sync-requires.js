b62f474bc90420ed2ab7c75005c31d597e5fbfe0
Tenzin <tenzin@tenzin.ca>
2021-04-06 13:46:13 -0400

    Gatsby!

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("A:\\www\\projects\\tenzin.blog\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("A:\\www\\projects\\tenzin.blog\\src\\pages\\404.js")),
+  "component---src-pages-index-js": preferDefault(require("A:\\www\\projects\\tenzin.blog\\src\\pages\\index.js"))
+}
+
